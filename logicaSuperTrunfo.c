115de6aebece08cfcd4b4379a838f84a65cf98e5
moreno0308 <WELLINGTONMORENO03081996@GMAIL.COM>
2025-07-25 20:39:51 +0000

    cadastra carta

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,6 +8,12 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    int codigo_da_cidade_1 = 1, codigo_da_cidade_2 = 2; //essas variaveis não serão informadas pelo usuario serão fixas
+    int populacao_1 = 1000, populacao_2 = 1000, pontos_turisticos_1 = 2, pontos_turisticos_2 = 2;//cadastro das vareaveis de população e ponto turisticos 
+    float area_1, area_2 = 000.00, PIB_1, PIB_2 = 00.00;// cadastro dos pontos flutuantes area e PIB
+    char  nome_1[25] = "Cidade 1", nome_2[25] = "Cidade 2"; //nomes das cidades
+    char estado_1 = 'A', estado_2 = 'B';
+    float SuperPoder_A = 15.00, SuperPoder_B = 15.00;
 
     
     // Cadastro das Cartas:
